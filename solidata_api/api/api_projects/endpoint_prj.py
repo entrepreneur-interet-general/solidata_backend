20438db25b4c7c3fd77bbd6ce5693ab28b4c3ba1
Julien_P <jparis.py@gmail.com>
2019-01-21 14:23:27 +0100

    update requirements.txt

--- solidata_api/api/api_projects/endpoint_prj.py
+++ solidata_api/api/api_projects/endpoint_prj.py
@@ -73,7 +73,7 @@ class Prj_infos_(Resource):
 
 
 		### query db from generic function 		
-		query_args				= query_data_arguments.parse_args(request)
+		query_args			= query_data_arguments.parse_args(request)
 		page_args				= pagination_arguments.parse_args(request)
 		results, response_code	= Query_db_doc (
 			ns, 
