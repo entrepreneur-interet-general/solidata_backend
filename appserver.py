ae4a9f6489db55bcab6841ca30180535a4903551
Julien_P <jparis.py@gmail.com>
2019-03-07 16:50:17 +0100

    testing approach to serve correct host to swagger and flask

--- appserver.py
+++ appserver.py
@@ -113,6 +113,8 @@ def app_runner(mode, host, port, https, rsa, anojwt) :
 	print("=== RUNNING APP "+ "==="*40)
 	print("=== "*40)
 	print()
+
+	log.debug("app.config : \n %s", pformat(app.config) )
 	host = app.config["DOMAIN_ROOT"]
 	port = app.config["DOMAIN_PORT"]
 	app.run( debug=app_debug, host=host, port=port, threaded=True )
