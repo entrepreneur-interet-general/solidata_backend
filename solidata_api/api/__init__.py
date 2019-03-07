c31855a232629d7f57dc4202cb11b6b4e0a0521f
Julien_P <jparis.py@gmail.com>
2019-03-07 06:09:46 +0100

    adding monkey patch to serve swagger on https too - test 15

--- solidata_api/api/__init__.py
+++ solidata_api/api/__init__.py
@@ -23,6 +23,17 @@ from 	werkzeug.security 	import 	generate_password_hash, check_password_hash
 
 from 	flask_restplus 		import Api, Namespace, Resource, fields, marshal, reqparse
 
+# monkey patch courtesy of
+# https://github.com/noirbizarre/flask-restplus/issues/54
+# so that /swagger.json is served over https
+if os.environ.get('HTTPS'):
+	@property
+	def specs_url(self):
+		"""Monkey patch for HTTPS"""
+		return url_for(self.endpoint('specs'), _external=True, _scheme='https')
+
+	Api.specs_url = specs_url
+
 # class MyApi(Api):
 # 	@property
 # 	def specs_url(self):
