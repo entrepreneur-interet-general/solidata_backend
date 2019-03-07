1df2b8899d25703c3b098e4e9cfa95284acb49c8
Julien_P <jparis.py@gmail.com>
2019-03-07 06:22:11 +0100

    adding monkey patch to serve swagger on https too - test 17 WTF

--- solidata_api/api/__init__.py
+++ solidata_api/api/__init__.py
@@ -24,35 +24,15 @@ from 	werkzeug.security 	import 	generate_password_hash, check_password_hash
 from 	flask_restplus 		import Api, Namespace, Resource, fields, marshal, reqparse
 
 # monkey patch courtesy of
-# https://github.com/noirbizarre/flask-restplus/issues/54
-# so that /swagger.json is served over https
-# if os.environ.get('HTTPS'):
-# 	@property
-# 	def specs_url(self):
-# 		"""Monkey patch for HTTPS"""
-# 		return url_for(self.endpoint('specs'), _external=True, _scheme='https')
-
-# 	Api.specs_url = specs_url
+# https://github.com/noirbizarre/flask-restplus/issues/565
 class MyApi(Api):
 	@property
 	def specs_url(self):
 		'''
 		The Swagger specifications absolute url (ie. `swagger.json`)
-
 		:rtype: str
 		'''
 		return url_for(self.endpoint('specs'), _external=False)
-# class MyApi(Api):
-# 	@property
-# 	def specs_url(self):
-# 		"""Monkey patch for HTTPS"""
-# 		# log.debug("self.base_url : %s" , self.base_url)
-# 		# log.debug("app.config['RUN_MODE'] : %s" , app.config["RUN_MODE"])
-# 		# scheme = 'http' if "http" in self.base_url else 'https'
-# 		# scheme = 'https' if app.config["RUN_MODE"] in ["prod","preprod"] else 'http'
-# 		# scheme = 'http' if app.config["ENV"] in ["dev"] or app.config["DOMAIN_PORT"] in self.base_url else 'https'
-# 		scheme = 'http' if app.config["DOMAIN_PORT"] in self.base_url else 'https'
-# 		return url_for(self.endpoint('specs'), _external=True, _scheme=scheme)
 
 import jwt
 from flask_jwt_extended import (
